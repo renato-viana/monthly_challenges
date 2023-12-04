337b569396121af75a35ead916f7f94ea8305787
Renato Borges Viana <renatoviana30@gmail.com>
2023-12-04 15:34:22 -0300

    Path Converters

--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,19 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+def monthly_challenge_by_number(request, month):
+    challenge_text = None
+
+    if month == 1:
+        challenge_text = month
+    elif month == 2:
+        challenge_text = month
+    elif month == 3:
+        challenge_text = month
+    else:
+        return HttpResponseNotFound("This number is not supported!")
+
+    return HttpResponse(challenge_text)
 
 def monthly_challenge(request, month):
     challenge_text = None
