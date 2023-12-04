909de0d58a3744e58acc8c6114a7f28cbadbf29d
Renato Borges Viana <renatoviana30@gmail.com>
2023-12-04 16:30:37 -0300

    The Reverse Function & Named URLs

--- challenges/views.py
+++ challenges/views.py
@@ -26,7 +26,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month!")
 
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
